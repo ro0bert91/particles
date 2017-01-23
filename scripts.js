0fc12bfbaff0c19ed3180c6cc739c0a7b134294b
ro0bert91 <patalas.robert1991@gmail.com>
2017-01-23 20:33:43 +0100

    canvas test

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 /**
  * Created by Robrty on 23.01.2017.
  */
+document.addEventListener('DOMContentLoaded',domloaded,false);
+function domloaded(){
+
+    var canvas = document.querySelector("#playground");
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillStyle = "blue";
+    ctx.arc(10, 10, 10, 0, Math.PI * 2);
+    ctx.fill();
+}
+
